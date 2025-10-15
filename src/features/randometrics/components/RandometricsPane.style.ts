850dc0f28b3c1306d5d2673d507686b0bd96ea8b
Greg <gregmichaeldavies@gmail.com>
2025-10-15 13:22:09 +0100

    feat: Improved Randometrics styling.

---
+++ src/features/randometrics/components/RandometricsPane.style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const RandoMetricsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
--- src/features/randometrics/components/RandometricsPane.tsx
+++ src/features/randometrics/components/RandometricsPane.tsx
@@ -1,10 +1,12 @@
-import { MetricChip, Pane } from "@gergling/ui-components"
+import { MetricChip } from "@gergling/ui-components"
 import { useRandometricDevelopmentProgress } from "../hooks"
 import { useMemo } from "react";
 import { MetricChipProps } from "../types";
 import { FACETOMETRICS } from "../constants";
 import { Temporal } from "@js-temporal/polyfill";
 import { getSeasonalFacetometric } from "../utilities";
+import { RandoMetricsContainer } from "./RandometricsPane.style";
+import { PaneContainer } from "../../../common/components/PaneContainer";
 
 export const RandometricsPane = () => {
   const now = useMemo(() => Temporal.Now.zonedDateTimeISO(), []);
@@ -18,16 +20,17 @@ export const RandometricsPane = () => {
     ];
   }, [mvp, seasonal])
   return (
-    <Pane>
-      {metrics.map((metric) => (
-        <MetricChip
-          key={metric.label}
-          color={metric.color}
-          label={metric.label}
-          value={metric.value}
-        />
-      ))}
-    </Pane>
-
-  )
+    <PaneContainer>
+      <RandoMetricsContainer>
+        {metrics.map((metric) => (
+          <MetricChip
+            key={metric.label}
+            color={metric.color}
+            label={metric.label}
+            value={metric.value}
+          />
+        ))}
+      </RandoMetricsContainer>
+    </PaneContainer>
+  );
 }
