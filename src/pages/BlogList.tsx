5edf033998503ea86a81e5029c897f60a2308881
Greg <gregmichaeldavies@gmail.com>
2025-10-26 12:46:29 +0000

    feat: Added SEO to team page.

--- src/pages/BlogList.tsx
+++ src/pages/BlogList.tsx
@@ -1,9 +1,14 @@
 import { AllBlogs } from "../features/blogs";
 import { PageContainer } from "../common/components/styles";
+import { Seo } from "../common/components/Seo";
 
 export const BlogListPage: React.FC = () => {
   return (
     <PageContainer>
+      <Seo
+        title="Blog Posts"
+        description="Gregory, Michael & Davies cruel and unusual measurements, ratings and categorisations presents: Blogs."
+      />
       <AllBlogs />
     </PageContainer>
   );
