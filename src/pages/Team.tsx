f54b546c4f5ef5c16beeed8d0eb71daf200b1a5c
Greg <gregmichaeldavies@gmail.com>
2025-10-26 12:45:21 +0000

    feat: Added SEO to team page.

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -1,9 +1,14 @@
+import { Seo } from "../common/components/Seo";
 import { PageContainer } from "../common/components/styles";
 import { Team } from "../features/team";
 
 export const TeamPage: React.FC = () => {
   return (
     <PageContainer>
+      <Seo
+        title="Meet The Team"
+        description="Meet the Team at Gregory, Michael & Davies' cruel and unusual measurements, ratings and categorisations."
+      />
       <Team />
     </PageContainer>
   );
