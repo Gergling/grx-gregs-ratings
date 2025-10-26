330543cc8f5405d555859576004b17184b0e233c
Greg <gregmichaeldavies@gmail.com>
2025-10-26 19:51:42 +0000

    tech: Imported image properly.

--- src/common/components/Seo.tsx
+++ src/common/components/Seo.tsx
@@ -2,16 +2,14 @@ import React, { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import { SITE_NAME, URL_ROOT } from '../constants';
 import { SeoProps } from '../types';
-
-const ENV_BASE_URL = import.meta.env.BASE_URL;
-const META_IMAGE_URL = new URL(`${ENV_BASE_URL}assets/images/smiling-man-pointing-left.jpg`, URL_ROOT).href;
+import smilingManPointLeftImage from "../../assets/images/smiling-man-pointing-left.jpg";
 
 export const Seo: React.FC<SeoProps> = ({
   title,
   description,
   siteName = SITE_NAME,
   url: urlProp,
-  image = META_IMAGE_URL,
+  image = smilingManPointLeftImage,
   twitterHandle,
 }) => {
   const { pathname } = useLocation();
